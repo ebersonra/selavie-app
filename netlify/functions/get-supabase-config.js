6a1149bbfe01904d2732f687fb84670532deb347
Eberson Ramos <ebersonramos11@gmail.com>
2025-06-02 03:08:00 -0300

    Ajustando erro tela de admin

--- netlify/functions/get-supabase-config.js
+++ netlify/functions/get-supabase-config.js
@@ -3,10 +3,11 @@ exports.handler = async function() {
         statusCode: 200,
         headers: {
             'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
         },
         body: JSON.stringify({
             supabaseUrl: process.env.SUPABASE_URL,
-            supabaseKey: process.env.SUPABASE_SERVICE_ROLE_KEY
+            supabaseKey: process.env.SUPABASE_ANON_KEY
         })
     };
 }; 
